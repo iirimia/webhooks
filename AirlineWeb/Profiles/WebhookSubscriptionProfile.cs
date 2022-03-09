96b4b0e47853a3295c886ff68b45badd688a056f
iirimia <ionela.irimia@gmail.com>
2022-03-09 15:08:49 +0000

    Setup airlineweb

---
+++ AirlineWeb/Profiles/WebhookSubscriptionProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using AirlineWeb.Dtos;
+using AirlineWeb.Models;
+
+namespace AirlineWeb.Profiles
+{
+    public class WebhookSubscriptionProfile: Profile
+    {
+        public WebhookSubscriptionProfile()
+        {
+            //Source -> Target
+            CreateMap<WebhookSubscriptionCreateDto, WebhookSubscription>();
+            CreateMap<WebhookSubscription, WebhookSubscriptionReadDto>();
+              
+        }
+    }
+}
--- AirlineWeb/Program.cs
+++ AirlineWeb/Program.cs
@@ -1,11 +1,17 @@
+using AirlineWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("AirlineConnection");
+builder.Services.AddDbContext<AirlineDbContext>(opt => opt.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
